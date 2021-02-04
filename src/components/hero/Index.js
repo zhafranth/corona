de6932ec3978c2a0e52c995fbe64c9bc33712563
Zhafranth <zhafranth@gmail.com>
2021-02-04 23:07:01 +0700

    hero

--- src/components/hero/Index.js
+++ src/components/hero/Index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import {
   MainHero,
   HeroContent,
@@ -16,20 +17,56 @@ import {
   WrapperCases,
   Cases,
   People,
-  Wave,
+  BgHero,
+  BgCard,
+  BgVirus,
+  LoadCardTitle,
+  LoadCardIcon,
+  LoadCardCases,
+  LoadCardPeople,
 } from "./style";
+
+// Reveal
+import Bounce from "react-reveal/Bounce";
 
 // Icons
 import Icon1 from "assets/icons/icon1.svg";
 import Icon2 from "assets/icons/icon2.svg";
 import Icon3 from "assets/icons/icon3.svg";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
+
+// Images
+import ImgBgHero from "assets/images/bg-hero.png";
+import ImgBgCard from "assets/images/bg-card.png";
+import VirusBg from "assets/images/virusbg.png";
 
 const Hero = () => {
+  const [indonesia, setIndonesia] = useState([]);
+  const [jakarta, setJakarta] = useState([]);
+  const [recovered, setRecovered] = useState([]);
+  const [death, setDeath] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://api.kawalcorona.com/indonesia")
+      .then((res) => {
+        setIndonesia(res.data[0].positif);
+        setRecovered(res.data[0].sembuh);
+        setDeath(res.data[0].meninggal);
+      })
+      .catch((err) => console.log(err));
+    Axios.get("https://api.kawalcorona.com/indonesia/provinsi")
+      .then((res) => {
+        setJakarta(res.data[0].attributes.Kasus_Posi);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <MainHero>
-      {/* <Wave xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path d="M0,224L60,234.7C120,245,240,267,360,256C480,245,600,203,720,160C840,117,960,75,1080,74.7C1200,75,1320,117,1380,138.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
-      </Wave> */}
+      <Bounce left delay={300} speed="2000s">
+        <BgVirus src={VirusBg} />
+      </Bounce>
+      <BgHero src={ImgBgHero} />
       <HeroContent>
         <TitleContent>Stay Home Stay Safe</TitleContent>
         <DescContent>
@@ -45,50 +82,74 @@ const Hero = () => {
         </DescTracker>
         <WrapperTracker>
           <Card>
+            <BgCard src={ImgBgCard} />
             <WrapperTitle>
-              <ImgWrapper color="#FFD7DF">
-                <ImgCover src={Icon1} />
-              </ImgWrapper>
-              <TitleCard>Total Cases</TitleCard>
+              {indonesia.length != 0 ? (
+                <ImgWrapper color="#FFD7DF">
+                  <ImgCover src={Icon1} />
+                </ImgWrapper>
+              ) : (
+                <LoadCardIcon />
+              )}
+              {indonesia.length != 0 ? (
+                <TitleCard>Total Cases</TitleCard>
+              ) : (
+                <LoadCardTitle />
+              )}
             </WrapperTitle>
-            <WrapperCases>
-              <Cases fontColor="#FD4B6E">1,231,223</Cases>
-            </WrapperCases>
-            <People>People</People>
+            {indonesia.length != 0 ? (
+              <WrapperCases>
+                <Cases fontColor="#FD4B6E">{indonesia}</Cases>
+                <BsFillCaretUpFill />
+              </WrapperCases>
+            ) : (
+              <LoadCardCases />
+            )}
+            {indonesia.length != 0 ? (
+              <People>People</People>
+            ) : (
+              <LoadCardPeople />
+            )}
           </Card>
           <Card>
+            <BgCard src={ImgBgCard} />
             <WrapperTitle>
               <ImgWrapper color="#FFDDC7">
                 <ImgCover src={Icon1} />
               </ImgWrapper>
-              <TitleCard>Total Cases</TitleCard>
+              <TitleCard>Total Cases Jakarta</TitleCard>
             </WrapperTitle>
             <WrapperCases>
-              <Cases fontColor="#FF6E0E">1,231,223</Cases>
+              <Cases fontColor="#FF6E0E">{jakarta}</Cases>
+              <BsFillCaretUpFill />
             </WrapperCases>
             <People>People</People>
           </Card>
           <Card>
+            <BgCard src={ImgBgCard} />
             <WrapperTitle>
               <ImgWrapper color="#EBFFD8">
                 <ImgCover src={Icon2} />
               </ImgWrapper>
-              <TitleCard>Total Cases</TitleCard>
+              <TitleCard>Recovered</TitleCard>
             </WrapperTitle>
             <WrapperCases>
-              <Cases fontColor="#7BD821">1,231,223</Cases>
+              <Cases fontColor="#7BD821">{recovered}</Cases>
+              <BsFillCaretUpFill />
             </WrapperCases>
             <People>People</People>
           </Card>
           <Card>
+            <BgCard src={ImgBgCard} />
             <WrapperTitle>
               <ImgWrapper color="#E8E1FF">
                 <ImgCover src={Icon3} />
               </ImgWrapper>
-              <TitleCard>Total Cases</TitleCard>
+              <TitleCard>Death</TitleCard>
             </WrapperTitle>
             <WrapperCases>
-              <Cases fontColor="#9B7DFF">1,231,223</Cases>
+              <Cases fontColor="#9B7DFF">{death}</Cases>
+              <BsFillCaretUpFill />
             </WrapperCases>
             <People>People</People>
           </Card>
--- src/components/hero/style.js
+++ src/components/hero/style.js
@@ -1,9 +1,14 @@
-import styled from "styled-components/macro";
+import styled, { keyframes } from "styled-components/macro";
 
+const shimmer = keyframes`
+  100% {
+      transform: translateX(100%);
+  }
+`;
 export const MainHero = styled.div`
   position: relative;
   padding: 4rem 12vw;
-  /* height: 88vh; */
+  height: 70vh;
   display: flex;
   background-color: #f4f9ff;
   box-sizing: border-box;
@@ -11,6 +16,7 @@ export const MainHero = styled.div`
 `;
 export const HeroContent = styled.div`
   width: 30%;
+  z-index: 2;
 `;
 export const TitleContent = styled.h2`
   font-weight: bold;
@@ -48,6 +54,8 @@ export const WrapperTracker = styled.div`
   gap: 2.1rem;
 `;
 export const Card = styled.div`
+  position: relative;
+  overflow: hidden;
   width: 45%;
   border-radius: 25px;
   background-color: #fff;
@@ -62,12 +70,12 @@ export const ImgWrapper = styled.div`
   border-radius: 50px;
   width: 50px;
   height: 50px;
-  background-color: ${(props) => props.color};
   margin-right: 16px;
+  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
+  background-color: ${(props) => props.color};
 `;
 export const ImgCover = styled.img`
   width: 30px;
@@ -80,22 +88,174 @@ export const TitleCard = styled.h5`
   font-weight: 600;
   color: #0d003a;
 `;
-export const WrapperCases = styled.div``;
+export const WrapperCases = styled.div`
+  display: flex;
+  align-items: center;
+`;
 export const Cases = styled.h2`
   font-size: 52px;
   font-weight: 700;
   margin: 0;
   color: ${(props) => props.fontColor};
 `;
-export const People = styled.p``;
+export const People = styled.p`
+  margin: 0;
+`;
 
-export const Wave = styled.svg`
+export const BgHero = styled.img`
   position: absolute;
-  bottom: -240px;
-  left: 0;
+  top: 0;
+  left: -40px;
+  z-index: 1;
+`;
+
+export const BgVirus = styled.img`
+  position: absolute;
+  top: 1.5rem;
+  left: 12vw;
+  z-index: 2;
+`;
+
+export const BgCard = styled.img`
+  position: absolute;
+  top: 0;
   right: 0;
-  z-index: -1;
-  path {
-    fill: #f4f9ff;
+`;
+
+// ======== Loading
+export const LoadCardPeople = styled.div`
+  border-radius: 10px;
+  position: relative;
+  background-color: #f0f0f0;
+  width: 70%;
+  height: 20px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform: translateX(-100%);
+    background: linear-gradient(
+      linear,
+      left top,
+      right top,
+      from(transparent),
+      color-stop(rgba(255, 255, 255, 0.7)),
+      to(transparent)
+    );
+
+    background: linear-gradient(
+      90deg,
+      transparent,
+      rgba(255, 255, 255, 0.7),
+      transparent
+    );
+
+    /* Adding animation */
+    animation: ${shimmer} 1s infinite;
+  }
+`;
+
+export const LoadCardCases = styled.div`
+  border-radius: 10px;
+  position: relative;
+  background-color: #f0f0f0;
+  width: 70%;
+  height: 50px;
+  margin: 1rem 0;
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform: translateX(-100%);
+    background: linear-gradient(
+      linear,
+      left top,
+      right top,
+      from(transparent),
+      color-stop(rgba(255, 255, 255, 0.7)),
+      to(transparent)
+    );
+
+    background: linear-gradient(
+      90deg,
+      transparent,
+      rgba(255, 255, 255, 0.7),
+      transparent
+    );
+
+    /* Adding animation */
+    animation: ${shimmer} 1s infinite;
+  }
+`;
+
+export const LoadCardIcon = styled.div`
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+  margin-right: 16px;
+  overflow: hidden;
+  position: relative;
+  background-color: #f0f0f0;
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform: translateX(-100%);
+    background: linear-gradient(
+      linear,
+      left top,
+      right top,
+      from(transparent),
+      color-stop(rgba(255, 255, 255, 0.7)),
+      to(transparent)
+    );
+
+    background: linear-gradient(
+      90deg,
+      transparent,
+      rgba(255, 255, 255, 0.7),
+      transparent
+    );
+
+    /* Adding animation */
+    animation: ${shimmer} 1s infinite;
+  }
+`;
+
+export const LoadCardTitle = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 50%;
+  height: 20px;
+  background-color: #f0f0f0;
+  border-radius: 15px;
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform: translateX(-100%);
+    background: linear-gradient(
+      linear,
+      left top,
+      right top,
+      from(transparent),
+      color-stop(rgba(255, 255, 255, 0.7)),
+      to(transparent)
+    );
+
+    background: linear-gradient(
+      90deg,
+      transparent,
+      rgba(255, 255, 255, 0.7),
+      transparent
+    );
+
+    /* Adding animation */
+    animation: ${shimmer} 1s infinite;
   }
 `;
