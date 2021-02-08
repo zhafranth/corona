be10f453f918722f2142777c4b1dfe8f69ff7543
Zhafranth <zhafranth@gmail.com>
2021-02-08 22:20:36 +0700

    service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html", "404.png"];
+
+const self = this;
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened Cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+// Lister for Request
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+// Activated
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
